080c0ad95da12a1db1076255cd8f96e29c9d9fb7
Fredrik Svard <fredrik.svard@gmail.com>
2018-05-21 21:44:51 +0200

    First attempt

---
+++ features/idirlion.feature
@@ -0,0 +1,5 @@
+Feature: Check your internet connection
+
+Scenario: Decide what to write here
+
+  Given that this works
---
+++ features/step_definitions/idirlion.rb
@@ -0,0 +1,4 @@
+
+Given("that this works") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
